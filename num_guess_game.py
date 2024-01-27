6158eb914302006b7fae3ea6e3b4222b808026b5
davidk0121 <66999088+davidk0121@users.noreply.github.com>
2024-01-27 13:00:36 -0800

    Update num_guess_game.py

--- num_guess_game.py
+++ num_guess_game.py
@@ -3,25 +3,24 @@ import random
 print("Number Guessing game")
 num_guess = int(input("Choose a number between 1 and 100: "))
 random_num = random.randint(1, 100)
-trials = 1
+trials = 0
 
 while True:
     if num_guess > random_num:
         print("Number should be lower!")
-        num_guess = input("Guess a number between 1 and 100: ")
-        num_guess = int(num_guess)
     elif num_guess < random_num:
         print("Number should be higher!")
-        num_guess = input("Guess a number between 1 and 100: ")
-        num_guess = int(num_guess)
     else:
         print(f"You got it right in {trials} tries!!! Good job!")
         ask_again = input("Do you want to play again? (y/n): ")
-        trials = 1
-        if ask_again == "y":
+        if ask_again.lower() == "y":
             random_num = random.randint(1, 100)
-            num_guessguess = None
+            num_guess = None
+            trials = 0  # Reset
         else:
             print("Thank you for playing! Bye!")
             break
+
     trials += 1
+    num_guess = input("Guess a number between 1 and 100: ")
+    num_guess = int(num_guess)
